54784b868f2d00495baef87a9020671ae0ea44f2
Len Yi <38734485+ellenpu251@users.noreply.github.com>
2020-07-02 19:09:18 -0500

    v02

---
+++ src/mayautils.py
@@ -0,0 +1,76 @@
+import logging
+import os
+import maya.cmds as cmds
+
+import pymel.core as pmc
+from pymel.core.system import Path
+
+from pymel.core.system import versions
+
+log = logging.getLogger(__name__)
+
+def main():
+    increment = 0
+    while(increment<3):
+        if increment >= 10:
+            break
+        cubexform, cubeshape = pmc.polyCube()
+        cubexform.translateT.set(1.5*increment)
+        increment+=1
+
+class SceneFile(object):
+    def __init__(self, dir="", descriptor='main', version=1, ext="ma"):
+        FilePath = cmds.file(q=True, sn=True)
+        if(FilePath==""):
+            self._dir = Path(dir)
+            self.descriptor = descriptor
+            self.version = version
+            self.ext = ext
+        else:
+            parts = os.path.split(FilePath)
+            self._dir = parts[0]
+            Name = parts[1].split('_v')
+            self.descriptor = Name[0]
+            Split2 = Name[1].split('.')
+            self.version = int(Split2[0])
+            self.ext = Split2[1]
+
+    @property
+    def dir(self,val):
+        self._dir=Path(val)
+
+    def basename(self):
+        name_pattern = "{descriptor}.{version:03d}.{ext}"
+        name = name_pattern.format(descriptor=self.descriptor, version=self.version, ext=self.ext)
+        return name
+
+    def path(self):
+        return Path(self.dir) / self.basename()
+
+    def save(self):
+        try:
+            Path = self.dir + "\\" + self.descriptor + '_v0' + str(self.version) + '.' +self.ext
+            pmc.system.saveAs(Path)
+        except RuntimeError:
+            log.warning("Missing directories.creating directories")
+            self.dir.markedirs_p()
+            Path = self.dir + "\\" + self.descriptor + '_v0' + str(self.version) + '.' + self.ext
+            pmc.system.saveAs(Path)
+
+    def increment_and_save(self):
+        CurrentVersion = self.version
+        for i in self.dir.files('*.ma'):
+            parts = os.path.split(i)
+            Directory = parts[0]
+            Name = parts[1].split('_v')
+            Descriptor = Name[0]
+            Split2 = Name[1].split('.')
+            Version = int(Split2[0])
+            Extension = Split2[1]
+            if(self.descriptor == Descriptor):
+                if CurrentVersion < Version:
+                    CurrentVersion = Version
+                CurrentVersion = CurrentVersion + 1
+                Path = self.dir + "\\" + self.descriptor + '_v0' + str(CurrentVersion) + '.' + self.ext
+                pmc.system.saveAs(Path)
+
---
+++ src/smartsaveui.py
@@ -0,0 +1,106 @@
+import maya.OpenMayaUI as omui
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+
+import mayautils
+
+def maya_main_window():
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+class SimpleUI(QtWidgets.QDialog):
+
+ def __init__(self):
+    super(SimpleUI,self).__init__(parent=maya_main_window())
+    self.scene = mayautils.SceneFile()
+    print(self.scene.dir)
+    print(self.scene.dir)
+    self.setWindowTitle("A Simple UI")
+    self.resize(500,200)
+    self.setWindowFlags(self.windowFlags()^QtCore.Qt.WindowContextHelpButtonHint)
+
+    self.create_widgets()
+    self.create_layout()
+    self.create_connections()
+
+ def create_widgets(self):
+    self.title_lbl = QtWidgets.QLabel("Smart Save")
+    self.title_lbl.setStyleSheet("font: bold 50px")
+    self.dir_lbl = QtWidgets.QLineEdit()
+    self.dir_le.setText(self.scene.dir)
+    self.browse_btn = QtWidgets.QPushButton("Browse")
+    self.descriptor_lbl = QtWidgets.QLineEdit()
+    self.descriptor_le.setText(self.scene.descriptor)
+    self.version_lbl = QtWidgets.QLabel("Version")
+    self.version_spinbox = QtWidgets.QSpinBox()
+    self.version_spinbox.setValue(self.scene.version)
+
+    self.ext_lbl = QtWidgets.QLabel("Extension")
+    self.ext_le = QtWidgets.QLineEdit()
+    self.ext_le.setText(self.scene.ext)
+    self.save_btn = QtWidgets.QPushButton("Increment and Save")
+    self.cancel_btn = QtWidgets.QPushButton("Cancel")
+
+    def create_layout(self):
+        self.directory_lay = QtWidgets.QHBoxLayout()
+        self.directory_lay.addWidget(self.dir_lbl)
+        self.directory_lay.addWidget(self.dir_le)
+        self.directory_lay.addWidget(self.browse_btn)
+
+        self.descriptor_lay = QtWidgets.QHBoxLayout()
+        self.descriptor_lay.addWidget(self.descriptor_lbl)
+        self.descriptor_lay.addWidget(self.descriptor_le)
+
+        self.version_lay = QtWidgets.QHBoxLayout()
+        self.version_lay.addWidget(self.version_lbl)
+        self.version_lay.addWidget(self.version_spinbox)
+
+        self.ext_lay = QtWidgets.QHBoxLayout()
+        self.ext_lay.addWidget(self.ext_lbl)
+        self.ext_lay.addWidget(self.ext_le)
+
+        self.bottom_btn_lay = QtWidgets.QHBoxLayout()
+        self.bottom_btn_lay.addWidget(self.save_btn)
+        self.bottom_btn_lay.addWidget(self.cancel_btn)
+
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.addWidget(self.title_lbl)
+        self.main_layout.addLayout(self.directory_lay)
+        self.main_layout.addLayout(self.descriptor_lay)
+        self.main_layout.addLayout(self.version_lay)
+        self.main_layout.addStretch()
+        self.main_layout.addLayout(self.bottom_btn_lay)
+        self.setLayout(self.main_layout)
+
+    def create_connections(self):
+        self.browse_btn.clicked.connect(self.browse)
+        self.cancel_btn.clicked.connect(self.cancel)
+        self.save_btn.clicked.connect(self.save)
+        self.save_incr_btn.clicked.connect(self.increment_and_save)
+
+    def _populate_scenefile_properties(self):
+        self.scene.dir = self.dir_le.text()
+        self.scene.descriptor = self.descriptor_le.text()
+        self.scene.version = self.version_spinbox.value()
+        self.scene.ext = self.ext_le.text()
+
+    @QtCore.Slot()
+    def save(self):
+        self._populate_scenefile_properties()
+        self.scene.save()
+
+    @QtCore.Slot()
+    def cancel(self):
+        self.close()
+
+    @QtCore.Slot()
+    def browse(self):
+        NewDirectory = QtWidgets.QFileDialog.getExistingDirectory()
+        self.scene.dir = NewDirectory
+        self.dir_le.setText(self.scene.dir)
+
+    @QtCore.Slot()
+    def increment_and_save(self):
+        self._populate_scenefile_properties()
+        self.scene.increment_and_save()
+
